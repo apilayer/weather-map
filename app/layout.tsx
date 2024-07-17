3a0ff06f01efe6d0ba71bcd13ee9aaf13be1f82f
Savio Martin <saviomartin2007@gmail.com>
2024-07-17 19:32:53 +0530

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
